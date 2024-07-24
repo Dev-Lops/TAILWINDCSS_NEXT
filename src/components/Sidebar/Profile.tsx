cb06097e20ce9ec2b9a099fa258d1ef19ce37a0e
Anderson Lopes <developes.anderson@gmail.com>
2024-07-24 00:14:06 -0400

    🚀 Projeto Finalizado

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,13 +1,10 @@
 import { LogOut } from 'lucide-react'
+import Image from 'next/image'
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
-      <img
-        src="https://github.com/lopesa95.png"
-        className="h-10 w-10 rounded-full"
-        alt=""
-      />
+    <div className="grid grid-cols-profile items-center gap-3">
+      <Image className="h-10 w-10 rounded-full" alt="logo" src="" />
       <div className="flex flex-col truncate">
         <span className="text-sm font-semibold text-zinc-700">
           Anderson Lopes
@@ -16,7 +13,11 @@ export function Profile() {
           develops@gmail.com
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-50"
+        title=""
+      >
         <LogOut className="h-5 w-5 text-zinc-500" />
       </button>
     </div>
